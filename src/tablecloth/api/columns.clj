ffb12eea272d240c930aa77a9df314ca084cbbe4
GenerateMe <generateme.blog@gmail.com>
2020-10-19 11:11:52 +0200

    -removed unnecessary function

--- src/tablecloth/api/columns.clj
+++ src/tablecloth/api/columns.clj
@@ -168,29 +168,25 @@
     (ds/select-columns ds (clojure.core/concat selected-cols rest-cols))))
 ;;
 
-(defn- try-convert-to-type
-  [ds colname new-type]
-  (ds/column-cast ds colname new-type))
-
 (defn- process-convert-column-type
   ([ds colname new-type]
    (if (iterable-sequence? new-type)
-     (try-convert-to-type ds colname new-type)
+     (ds/column-cast ds colname new-type)
      (cond
        (nil? new-type) (-> (ds/->dataset {colname (ds/column ds colname)})
                            (ds/column colname)
                            (->> (ds/add-or-update-column ds colname)))
-       (= :object new-type) (try-convert-to-type ds colname [:object identity])
+       (= :object new-type) (ds/column-cast ds colname [:object identity])
        :else (let [col (ds colname)]
                (condp = (dtype/get-datatype col)
                  :string (ds/add-or-update-column ds colname (col/parse-column new-type col))
                  :object (if (string? (dtype/get-value col 0))
-                           (-> (try-convert-to-type ds colname :string)
+                           (-> (ds/column-cast ds colname :string)
                                (ds/column colname)
                                (->> (col/parse-column new-type)
                                     (ds/add-or-update-column ds colname)))
-                           (try-convert-to-type ds colname new-type))
-                 (try-convert-to-type ds colname new-type)))))))
+                           (ds/column-cast ds colname new-type))
+                 (ds/column-cast ds colname new-type)))))))
 
 (defn convert-types
   "Convert type of the column to the other type."
