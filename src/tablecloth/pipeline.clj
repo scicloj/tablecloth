c72bdad0bef13e1bb81dc422255ce66d0830c563
GenerateMe <generateme.blog@gmail.com>
2021-02-05 16:35:51 +0100

    pipeline works on maps now

--- src/tablecloth/pipeline.clj
+++ src/tablecloth/pipeline.clj
@@ -1,7 +1,7 @@
 (ns tablecloth.pipeline
   "Linear pipeline operations."
   (:refer-clojure :exclude [group-by drop concat rand-nth first last shuffle])
-  (:require [tablecloth.api]))
+  (:require [tablecloth.api :as api]))
 
 (defmacro build-pipelined-function
   [f m]
@@ -10,7 +10,10 @@
        ~@(for [arg args
                :let [narg (mapv #(if (map? %) 'options %) arg)
                      [a & r] (split-with (partial not= '&) narg)]]
-           (list narg `(fn [ds#] (apply ~f ds# ~@a ~(rest r))))))))
+           (list narg `(fn [ds#]
+                         (let [ctx# (if (api/dataset? ds#)
+                                      {:dataset ds#} ds#)]
+                           (assoc ctx# :dataset (apply ~f (ctx# :dataset) ~@a ~(rest r))))))))))
 
 (def ^:private excludes #{'dataset 'write-csv! 'let-dataset})
 
@@ -65,3 +68,59 @@
                                    (symbol? op) (resolve op)
                                    :else op)]]
                      (apply v nparams)))))
+
+
+;; example
+
+
+(def DS (api/dataset {:V1 (take 9 (cycle [1 2]))
+                      :V2 (range 1 10)
+                      :V3 (take 9 (cycle [0.5 1.0 1.5]))
+                      :V4 (take 9 (cycle ["A" "B" "C"]))}))
+
+(select-columns :type/numerical)
+;; => #function[tablecloth.pipeline/select-columns/fn--43815]
+
+((select-columns :type/numerical) DS)
+;; => {:dataset _unnamed [9 3]:
+;;    | :V1 | :V2 | :V3 |
+;;    |-----|-----|-----|
+;;    |   1 |   1 | 0.5 |
+;;    |   2 |   2 | 1.0 |
+;;    |   1 |   3 | 1.5 |
+;;    |   2 |   4 | 0.5 |
+;;    |   1 |   5 | 1.0 |
+;;    |   2 |   6 | 1.5 |
+;;    |   1 |   7 | 0.5 |
+;;    |   2 |   8 | 1.0 |
+;;    |   1 |   9 | 1.5 |
+;;    }
+
+(let [p (pipeline (group-by :V1)
+                  (fold-by :V4)
+                  (ungroup))]
+  (p DS))
+;; => {:dataset _unnamed [6 4]:
+;;    | :V4 |   :V1 |   :V2 |       :V3 |
+;;    |-----|-------|-------|-----------|
+;;    |   B |   [1] |   [5] |     [1.0] |
+;;    |   C | [1 1] | [3 9] | [1.5 1.5] |
+;;    |   A | [1 1] | [1 7] | [0.5 0.5] |
+;;    |   B | [2 2] | [2 8] | [1.0 1.0] |
+;;    |   C |   [2] |   [6] |     [1.5] |
+;;    |   A |   [2] |   [4] |     [0.5] |
+;;    }
+
+(def pipeline-declaration [[:group-by :V1]
+                           [:unique-by ::unique-by-operation {:strategy :ctx/strategy}]
+                           [:ungroup {:add-group-as-column :from-V1}]])
+
+(def unique-by-operation (fn [m] (mod (:V2 m) 3)))
+
+(def pipeline-1 (->pipeline {:strategy vec} pipeline-declaration))
+(def pipeline-2 (->pipeline {:strategy set} pipeline-declaration))
+
+
+(pipeline-1 DS)
+
+(pipeline-2 {:dataset DS})
