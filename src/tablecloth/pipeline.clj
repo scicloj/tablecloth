6977de0c05487ef3b1fc20370f4bea2d2a2b5753
GenerateMe <generateme.blog@gmail.com>
2021-02-10 14:20:59 +0100

    metamorph deps

--- src/tablecloth/pipeline.clj
+++ src/tablecloth/pipeline.clj
@@ -1,7 +1,8 @@
 (ns tablecloth.pipeline
   "Linear pipeline operations."
   (:refer-clojure :exclude [group-by drop concat rand-nth first last shuffle])
-  (:require [tablecloth.api :as api]))
+  (:require [tablecloth.api :as api]
+            [scicloj.metamorph.core :as mm]))
 
 (defmacro build-pipelined-function
   [f m]
@@ -12,12 +13,12 @@
                      [a & r] (split-with (partial not= '&) narg)]]
            (list narg `(fn [ds#]
                          (let [ctx# (if (api/dataset? ds#)
-                                      {:dataset ds#} ds#)]
-                           (assoc ctx# :dataset (apply ~f (ctx# :dataset) ~@a ~(rest r))))))))))
+                                      {:metamorph/data ds#} ds#)]
+                           (assoc ctx# :metamorph/data (apply ~f (ctx# :metamorph/data) ~@a ~(rest r))))))))))
 
-(def ^:private excludes #{'dataset 'write-csv! 'let-dataset})
+(def ^:private excludes '#{dataset write-csv! let-dataset row-count column-count})
 
-(defmacro process-all-api-symbols
+(defmacro ^:private process-all-api-symbols
   []
   (let [ps (ns-publics 'tablecloth.api)]
     `(do ~@(for [[f v] ps
@@ -28,55 +29,8 @@
 
 (process-all-api-symbols)
 
-(defn pipeline
-  [& ops]
-  (let [ops-with-uuid (map #(vector (java.util.UUID/randomUUID) %) ops)]
-    (fn [ctx]
-      (let [ctx (if (api/dataset? ctx)
-                  {:dataset ctx} ctx)]
-        (reduce (fn [curr-ctx [uuid op]]
-                  (-> curr-ctx
-                      (assoc :uuid uuid)
-                      (op)
-                      (dissoc :uuid))) ctx ops-with-uuid)))))
-
-(declare process-param)
-
-(defn- process-map
-  [ctx params]
-  (into {} (map (fn [[k v]]
-                  [k (process-param ctx v)]) params)))
-
-(defn- process-seq
-  [ctx params]
-  (mapv (fn [p] (process-param ctx p)) params))
-
-(defn- process-param
-  [ctx p]
-  (cond
-    (and (keyword? p)
-         (namespace p)
-         (not (#{"type" "!type"} (namespace p)))) (let [n (namespace p)]
-                                                    (if (= n "ctx")
-                                                      (ctx (keyword (name p)))
-                                                      (var-get (resolve (symbol p)))))
-    (map? p) (process-map ctx p)
-    (sequential? p) (process-seq ctx p)
-    :else p))
-
-(defn ->pipeline
-  ([ops] (->pipeline {} ops))
-  ([ctx ops]
-   (apply pipeline (for [[op & params] ops
-                         :let [nparams (process-param ctx params)
-                               v (cond
-                                   (and (keyword? op)
-                                        (not (namespace op))) (var-get (resolve (symbol "tablecloth.pipeline" (name op))))
-                                   (keyword? op) (var-get (resolve (symbol op)))
-                                   (symbol? op) (resolve op)
-                                   :else op)]]
-                     (apply v nparams)))))
-
+(def pipeline mm/pipeline)
+(def ->pipeline mm/->pipeline)
 
 ;; TODO: remove before releasing
 ;; example
@@ -91,7 +45,7 @@
 ;; => #function[tablecloth.pipeline/select-columns/fn--43815]
 
 ((select-columns :type/numerical) DS)
-;; => {:dataset _unnamed [9 3]:
+;; => #:metamorph{:data _unnamed [9 3]:
 ;;    | :V1 | :V2 | :V3 |
 ;;    |-----|-----|-----|
 ;;    |   1 |   1 | 0.5 |
@@ -105,19 +59,19 @@
 ;;    |   1 |   9 | 1.5 |
 ;;    }
 
-(let [p (pipeline (group-by :V1)
-                  (fold-by :V4)
-                  (ungroup))]
+(let [p (mm/pipeline (group-by :V1)
+                     (fold-by :V4)
+                     (ungroup))]
   (p DS))
-;; => {:dataset _unnamed [6 4]:
-;;    | :V4 |   :V1 |   :V2 |       :V3 |
-;;    |-----|-------|-------|-----------|
-;;    |   B |   [1] |   [5] |     [1.0] |
-;;    |   C | [1 1] | [3 9] | [1.5 1.5] |
-;;    |   A | [1 1] | [1 7] | [0.5 0.5] |
-;;    |   B | [2 2] | [2 8] | [1.0 1.0] |
-;;    |   C |   [2] |   [6] |     [1.5] |
-;;    |   A |   [2] |   [4] |     [0.5] |
+;; => #:metamorph{:data _unnamed [6 5]:
+;;    | :V4 |   :V1 |   :V2 |       :V3 | :metamorph/id |
+;;    |-----|-------|-------|-----------|---------------|
+;;    |   B |   [1] |   [5] |     [1.0] |           [0] |
+;;    |   C | [1 1] | [3 9] | [1.5 1.5] |         [0 0] |
+;;    |   A | [1 1] | [1 7] | [0.5 0.5] |         [0 0] |
+;;    |   B | [2 2] | [2 8] | [1.0 1.0] |         [0 0] |
+;;    |   C |   [2] |   [6] |     [1.5] |           [0] |
+;;    |   A |   [2] |   [4] |     [0.5] |           [0] |
 ;;    }
 
 (def pipeline-declaration [[:group-by :V1]
@@ -126,30 +80,50 @@
 
 (def unique-by-operation (fn [m] (mod (:V2 m) 3)))
 
-(def pipeline-1 (->pipeline {:strategy vec} pipeline-declaration))
-(def pipeline-2 (->pipeline {:strategy set} pipeline-declaration))
+(def pipeline-1 (mm/->pipeline {:strategy vec} pipeline-declaration))
+(def pipeline-2 (mm/->pipeline {:strategy set} pipeline-declaration))
 
 
 (pipeline-1 DS)
+;; => #:metamorph{:data _unnamed [6 6]:
+;;    | :from-V1 |   :V1 |   :V2 |       :V3 |       :V4 | :metamorph/id |
+;;    |----------|-------|-------|-----------|-----------|---------------|
+;;    |        1 | [1 1] | [3 9] | [1.5 1.5] | ["C" "C"] |         [0 0] |
+;;    |        1 | [1 1] | [1 7] | [0.5 0.5] | ["A" "A"] |         [0 0] |
+;;    |        1 |   [1] |   [5] |     [1.0] |     ["B"] |           [0] |
+;;    |        2 |   [2] |   [6] |     [1.5] |     ["C"] |           [0] |
+;;    |        2 |   [2] |   [4] |     [0.5] |     ["A"] |           [0] |
+;;    |        2 | [2 2] | [2 8] | [1.0 1.0] | ["B" "B"] |         [0 0] |
+;;    }
 
-(pipeline-2 {:dataset DS})
-
+(pipeline-2 {:metamorph/data DS})
+;; => #:metamorph{:data _unnamed [6 5]:
+;;    | :from-V1 |  :V1 |    :V2 |    :V3 |    :V4 |
+;;    |----------|------|--------|--------|--------|
+;;    |        1 | #{1} | #{3 9} | #{1.5} | #{"C"} |
+;;    |        1 | #{1} | #{7 1} | #{0.5} | #{"A"} |
+;;    |        1 | #{1} |   #{5} | #{1.0} | #{"B"} |
+;;    |        2 | #{2} |   #{6} | #{1.5} | #{"C"} |
+;;    |        2 | #{2} |   #{4} | #{0.5} | #{"A"} |
+;;    |        2 | #{2} | #{2 8} | #{1.0} | #{"B"} |
+;;    }
 
 ;; custom pipeline operation
 
 (defn transformation-returning-context []
-  (fn [{:keys [uuid] :as ctx}]
-    (if-let [my-data (ctx uuid)]
-      (do
-        (println "My stored data is: " my-data)
-        (update ctx uuid inc))
-      (assoc ctx uuid 0))))
+  (fn [ctx]
+    (let [id (ctx :metamorph/id)]
+      (if-let [my-data (ctx id)]
+        (do
+          (println "My stored data is: " my-data)
+          (update ctx id inc))
+        (assoc ctx id 0)))))
 
 (def do-something
-  (pipeline (transformation-returning-context)
-            (transformation-returning-context)))
+  (mm/pipeline (transformation-returning-context)
+               (transformation-returning-context)))
 
-(take 3 (rest (iterate do-something (api/dataset))))
+(take 3 (rest (iterate do-something {})))
 ;; => ({:dataset _unnamed [0 0],
 ;;      #uuid "b3d63d87-096b-4c17-8290-9f4fdd68d6a6" 0,
 ;;      #uuid "95a154e8-cdf0-4f7c-80e7-d0d3e00e1463" 0}My stored data is:  0
