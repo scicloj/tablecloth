d409629702c1435b74e654d976ce1949bf07cd60
ashimapanjwani <ashimapanjwani@gmail.com>
2021-03-05 17:31:05 +0530

    cleaned up the unit tests

--- test/tablecloth/api/columns_test.clj
+++ test/tablecloth/api/columns_test.clj
@@ -1,7 +1,9 @@
 (ns tablecloth.api.columns-test
   (:require [tablecloth.api :as api]
             [tech.v3.datatype :as dtype]
-            [midje.sweet :refer [tabular fact =>]]))
+            [midje.sweet :refer [tabular fact => =not=>]]
+            [tablecloth.common-test :refer [cloned-reader-ds non-cloned-reader-ds]]
+            [tech.v3.datatype.functional :refer [pow]]))
 
 
 ;; https://github.com/scicloj/tablecloth/issues/9
@@ -55,3 +57,70 @@
                :int64   1
                :float64 1.0
                :string  "abc"))
+
+(fact "add-column" (as-> cloned-reader-ds $
+                         (api/add-column $ :z (dtype/make-reader :float32 5 (rand)))
+                         (fact "column values must match by default"
+                               (first ($ :z)) => (first ($ :z)))))
+
+(fact "add-column" (as-> cloned-reader-ds $
+                         (api/add-column $ :z (dtype/make-reader :float32 5 (rand))
+                                     {:prevent-clone? true})
+                         (fact "column values must NOT match when :prevent-clone? is true"
+                               (first ($ :z)) =not=> (first ($ :z)))))
+
+(fact "add-columns" (as-> cloned-reader-ds $
+                          (api/add-columns $ {:c (dtype/make-reader :float32 5 (rand))
+                                          :d (dtype/make-reader :float32 5 (rand-int 10))})
+                          (fact "column values must match by default"
+                                (first ($ :c)) => (first ($ :c))
+                                (first ($ :d)) => (first ($ :d)))))
+
+(fact "add-columns" (as-> cloned-reader-ds $
+                          (api/add-columns $ {:c (dtype/make-reader :float32 5 (rand))
+                                          :d (dtype/make-reader :float32 5 (rand-int 10))}
+                                       {:prevent-clone? true})
+                          (fact "column values must NOT match when :prevent-clone? is true"
+                                (first ($ :c)) =not=> (first ($ :c))
+                                (first ($ :d)) =not=> (first ($ :d)))))
+
+(fact "map-columns" (as-> cloned-reader-ds $
+                          (api/map-columns $ :z
+                                       (api/column-names $ #{:float32} :datatype)
+                                       (fn [& rows]
+                                         (rand)))
+                          (fact "column values must match by default"
+                                (first ($ :z)) => (first ($ :z)))))
+
+(fact "map-columns" (as-> cloned-reader-ds $
+                          (api/map-columns $ :z
+                                       (api/column-names $ #{:float32} :datatype)
+                                       (fn [& rows]
+                                         (rand))
+                                       {:prevent-clone? true})
+                          (fact "column values must NOT match when :prevent-clone? is true"
+                                (first ($ :z)) =not=> (first ($ :z)))))
+
+(fact "update-columns with columns-map" (as-> non-cloned-reader-ds $
+                                              (api/update-columns $ {:y #(pow % 2)})
+                                              (fact "column values must match by default"
+                                                    (first ($ :y)) => (first ($ :y)))))
+
+(fact "update-columns with columns-map" (as-> non-cloned-reader-ds $
+                                              (api/update-columns $ {:y #(pow % 2)}
+                                                              {:prevent-clone? true})
+                                              (fact "column values must NOT match when :prevent-clone? is true"
+                                                    (first ($ :y)) =not=> (first ($ :y)))))
+
+(fact "update-columns with columns-selector" (as-> non-cloned-reader-ds $
+                                                   (api/update-columns $ :all #(pow % 2))
+                                                   (fact "column values must match by default"
+                                                         (first ($ :x)) => (first ($ :x))
+                                                         (first ($ :y)) => (first ($ :y)))))
+
+(fact "update-columns with columns-selector" (as-> non-cloned-reader-ds $
+                                                   (api/update-columns $ :all #(pow % 2)
+                                                                   {:prevent-clone? true})
+                                                   (fact "column values must NOT match when :prevent-clone? is true"
+                                                         (first ($ :x)) =not=> (first ($ :x))
+                                                         (first ($ :y)) =not=> (first ($ :y)))))
--- test/tablecloth/common_test.clj
+++ test/tablecloth/common_test.clj
@@ -1,11 +1,20 @@
 (ns tablecloth.common-test
-  (:require [tablecloth.api :as api]))
+  (:require [tablecloth.api :as api]
+            [tech.v3.datatype :as dtype]))
 
 (def DS (api/dataset {:V1 (take 9 (cycle [1 2]))
                       :V2 (range 1 10)
                       :V3 (take 9 (cycle [0.5 1.0 1.5]))
                       :V4 (take 9 (cycle ["A" "B" "C"]))}
                      {:dataset-name "DS"}))
+
+(def cloned-reader-ds (-> {:x (dtype/make-reader :float32 5 (rand))
+                           :y (dtype/make-reader :float32 5 (rand))}
+                          (api/dataset)))
+
+(def non-cloned-reader-ds (-> {:x (dtype/make-reader :float32 5 (rand))
+                               :y (dtype/make-reader :float32 5 (rand))}
+                              (api/dataset {:prevent-clone? true})))
 
 (defn approx
   ^double [^double v]
