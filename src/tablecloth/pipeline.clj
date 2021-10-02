665b78892a0f31cf835c9f09613821822c311e37
Carsten Behring <carsten.behring@gmail.com>
2021-10-03 00:38:25 +0200

    added reference to original to lifted functions metadata

--- src/tablecloth/pipeline.clj
+++ src/tablecloth/pipeline.clj
@@ -6,7 +6,7 @@
 (defmacro build-pipelined-function
   [f m]
   (let [args (map (comp vec rest) (:arglists m))]
-    `(defn ~(symbol (name f)) {:doc ~(:doc m)}
+    `(defn ~(symbol (name f)) {:doc ~(:doc m) :orig (symbol (var ~f))}
        ~@(for [arg args
                :let [narg (mapv #(if (map? %) 'options %) arg)
                      [a & r] (split-with (partial not= '&) narg)]]
