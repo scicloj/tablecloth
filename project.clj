949566cd7f8cb4fd2449e08ac13f02758dd797e5
GenerateMe <generateme.blog@gmail.com>
2022-06-10 17:06:58 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scicloj/tablecloth "6.076"
+(defproject scicloj/tablecloth "6.088"
   :description "Dataset manipulation library built on the top of tech.ml.dataset."
   :url "https://github.com/scicloj/tablecloth"
   :license {:name "The MIT Licence"
