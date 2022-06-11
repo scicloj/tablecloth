fb07581439860609ec806510bf27607532980f15
Ethan Miller <ethanzanemiller@gmail.com>
2022-06-11 11:44:22 +0300

    Polishing up existing column fns
    * added some docstrings
    * re-organized a little

--- src/tablecloth/api/column.clj
+++ src/tablecloth/api/column.clj
@@ -5,32 +5,31 @@
             [tech.v3.datatype :as dtype]))
 
 (defn column
+  "Create a `column` from a vector or sequence. "
   ([] (col/new-column nil []))
   ([data]
    (column data {:name nil}))
   ([data {:keys [name]}]
    (col/new-column name data)))
 
+;; Alias for tech.v3.dasetset.column.is-column?
+(defn column? 
+  "Return true or false `item` is a column."
+  [item]
+  (col/is-column? item))
 
-(defn column? [item]
-  (= (class item) tech.v3.dataset.impl.column.Column))
-
-(defn zeros [n-zeros]
-  (column (repeatedly n-zeros (constantly 0))))
-
-(defn ones [n-ones]
-  (column (repeatedly n-ones (constantly 1))))
-
-(defn typeof [col]
+;; Alias for tech.v3.datatype.elemwise-datatype`
+(defn typeof
+  "Returns the datatype fo the elements within the column `col`."
+  [col]
   (dtype/elemwise-datatype col))
 
+(defn zeros
+  "Create a new column filled wth `n-zeros`."
+  [n-zeros]
+  (column (repeatedly n-zeros (constantly 0))))
 
-
-(comment
-  (-> [1 2 3 4]
-      (col/new-column )
-      meta)
-
-  (def b (col/parse-column :string [1 2 3]))
-
-  )
+(defn ones
+  "Creates a new column filled with `n-ones`"
+  [n-ones]
+  (column (repeatedly n-ones (constantly 1))))
