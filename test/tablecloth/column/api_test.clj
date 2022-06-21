186e7643e0a05a24fdc8398310806b72c6016559
Ethan Miller <ethanzanemiller@gmail.com>
2022-06-21 15:49:39 +0300

    Add tests for `zeros` and `ones`

--- test/tablecloth/column/api_test.clj
+++ test/tablecloth/column/api_test.clj
@@ -10,3 +10,9 @@
       (api/column [1 2 3]) => [1 2 3]
       (api/column (list 1 2 3)) =>[1 2 3]
       (api/column (range 3)) => [0 1 2])
+
+(fact "`zeros` returns a column filled with zeros"
+      (api/zeros 3) => [0 0 0])
+
+(fact "`ones` returns a column filled with ones"
+      (api/ones 3) => [1 1 1])
