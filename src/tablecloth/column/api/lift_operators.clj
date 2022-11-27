3a9cc03a2bcae6fb95cc6e9d1969c7b5ad3fdd01
Ethan Miller <ethanzanemiller@gmail.com>
2022-11-27 15:26:20 -0500

    Lift some more fns

--- src/tablecloth/column/api/lift_operators.clj
+++ src/tablecloth/column/api/lift_operators.clj
@@ -9,6 +9,20 @@
     '>=
     '<
     '<=] lift-op
+   ['kurtosis
+    'sum
+    'mean
+    'skew
+    'variance
+    'standard-deviation
+    'quartile-3
+    'quartile-1
+    'median] (fn [fn-sym fn-meta]
+              (lift-op
+               fn-sym fn-meta
+               {:new-args '([col] [col options])
+                :new-args-lookup {'data 'col
+                                  'options 'options}}))
    ['percentiles] (fn [fn-sym fn-meta]
                     (lift-op
                      fn-sym fn-meta
@@ -21,6 +35,7 @@
   (reduce (fn [m [symlist liftfn]]
             (loop [syms symlist
                    result m]
+
               (if (empty? syms)
                 result
                 (recur (rest syms) (assoc result (first syms) liftfn)))))
@@ -28,6 +43,12 @@
           serialized-lift-fn-lookup))
 
 (comment
+  (def fun-mappings (ns-publics 'tech.v3.datatype.functional))
+
+  (-> fun-mappings
+      (get 'kurtosis)
+      meta)
+
   (do-lift (deserialize-lift-fn-lookup)
            'tablecloth.column.api.operators
            'tech.v3.datatype.functional
--- src/tablecloth/column/api/operators.clj
+++ src/tablecloth/column/api/operators.clj
@@ -4,142 +4,286 @@
  (:refer-clojure :exclude [+ - / > >= < <=]))
 
 (defn
- +
+ kurtosis
  ""
- ([x]
+ ([col]
   (let
-   [original-result__43655__auto__ (tech.v3.datatype.functional/+ x)]
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/kurtosis col)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([x y]
+    original-result__42776__auto__)))
+ ([col options]
   (let
-   [original-result__43655__auto__ (tech.v3.datatype.functional/+ x y)]
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/kurtosis col options)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([x y & args]
-  (let
-   [original-result__43655__auto__
-    (clojure.core/apply tech.v3.datatype.functional/+ x y args)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__))))
-
-(defn
- -
- ""
- ([x]
-  (let
-   [original-result__43655__auto__ (tech.v3.datatype.functional/- x)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([x y]
-  (let
-   [original-result__43655__auto__ (tech.v3.datatype.functional/- x y)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([x y & args]
-  (let
-   [original-result__43655__auto__
-    (clojure.core/apply tech.v3.datatype.functional/- x y args)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__))))
-
-(defn
- /
- ""
- ([x]
-  (let
-   [original-result__43655__auto__ (tech.v3.datatype.functional// x)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([x y]
-  (let
-   [original-result__43655__auto__ (tech.v3.datatype.functional// x y)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([x y & args]
-  (let
-   [original-result__43655__auto__
-    (clojure.core/apply tech.v3.datatype.functional// x y args)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__))))
-
-(defn
- >
- ""
- ([lhs mid rhs]
-  (let
-   [original-result__43655__auto__
-    (tech.v3.datatype.functional/> lhs mid rhs)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([lhs rhs]
-  (let
-   [original-result__43655__auto__
-    (tech.v3.datatype.functional/> lhs rhs)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__))))
-
-(defn
- >=
- ""
- ([lhs mid rhs]
-  (let
-   [original-result__43655__auto__
-    (tech.v3.datatype.functional/>= lhs mid rhs)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
- ([lhs rhs]
-  (let
-   [original-result__43655__auto__
-    (tech.v3.datatype.functional/>= lhs rhs)]
-   (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__))))
+    original-result__42776__auto__))))
 
 (defn
  <
  ""
  ([lhs mid rhs]
   (let
-   [original-result__43655__auto__
+   [original-result__42777__auto__
     (tech.v3.datatype.functional/< lhs mid rhs)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
+    original-result__42777__auto__)))
  ([lhs rhs]
   (let
-   [original-result__43655__auto__
+   [original-result__42777__auto__
     (tech.v3.datatype.functional/< lhs rhs)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__))))
+    original-result__42777__auto__))))
+
+(defn
+ sum
+ "Double sum of data using\n  [Kahan compensated summation](https://en.wikipedia.org/wiki/Kahan_summation_algorithm)."
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/sum col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/sum col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
 
 (defn
  <=
  ""
  ([lhs mid rhs]
   (let
-   [original-result__43655__auto__
+   [original-result__42777__auto__
     (tech.v3.datatype.functional/<= lhs mid rhs)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__)))
+    original-result__42777__auto__)))
  ([lhs rhs]
   (let
-   [original-result__43655__auto__
+   [original-result__42777__auto__
     (tech.v3.datatype.functional/<= lhs rhs)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43655__auto__))))
+    original-result__42777__auto__))))
+
+(defn
+ mean
+ "double mean of data"
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/mean col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/mean col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
+
+(defn
+ >
+ ""
+ ([lhs mid rhs]
+  (let
+   [original-result__42777__auto__
+    (tech.v3.datatype.functional/> lhs mid rhs)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([lhs rhs]
+  (let
+   [original-result__42777__auto__
+    (tech.v3.datatype.functional/> lhs rhs)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__))))
+
+(defn
+ -
+ ""
+ ([x]
+  (let
+   [original-result__42777__auto__ (tech.v3.datatype.functional/- x)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([x y]
+  (let
+   [original-result__42777__auto__ (tech.v3.datatype.functional/- x y)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([x y & args]
+  (let
+   [original-result__42777__auto__
+    (clojure.core/apply tech.v3.datatype.functional/- x y args)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__))))
+
+(defn
+ skew
+ ""
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/skew col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/skew col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
+
+(defn
+ variance
+ ""
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/variance col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/variance col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
+
+(defn
+ standard-deviation
+ ""
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/standard-deviation col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/standard-deviation col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
+
+(defn
+ /
+ ""
+ ([x]
+  (let
+   [original-result__42777__auto__ (tech.v3.datatype.functional// x)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([x y]
+  (let
+   [original-result__42777__auto__ (tech.v3.datatype.functional// x y)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([x y & args]
+  (let
+   [original-result__42777__auto__
+    (clojure.core/apply tech.v3.datatype.functional// x y args)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__))))
+
+(defn
+ >=
+ ""
+ ([lhs mid rhs]
+  (let
+   [original-result__42777__auto__
+    (tech.v3.datatype.functional/>= lhs mid rhs)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([lhs rhs]
+  (let
+   [original-result__42777__auto__
+    (tech.v3.datatype.functional/>= lhs rhs)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__))))
+
+(defn
+ quartile-3
+ ""
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/quartile-3 col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/quartile-3 col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
+
+(defn
+ quartile-1
+ ""
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/quartile-1 col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/quartile-1 col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
+
+(defn
+ +
+ ""
+ ([x]
+  (let
+   [original-result__42777__auto__ (tech.v3.datatype.functional/+ x)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([x y]
+  (let
+   [original-result__42777__auto__ (tech.v3.datatype.functional/+ x y)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__)))
+ ([x y & args]
+  (let
+   [original-result__42777__auto__
+    (clojure.core/apply tech.v3.datatype.functional/+ x y args)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42777__auto__))))
+
+(defn
+ median
+ ""
+ ([col]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/median col)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__)))
+ ([col options]
+  (let
+   [original-result__42776__auto__
+    (tech.v3.datatype.functional/median col options)]
+   (tablecloth.column.api.utils/return-scalar-or-column
+    original-result__42776__auto__))))
 
 (defn
  percentiles
  "Create a reader of percentile values, one for each percentage passed in.\n  Estimation types are in the set of #{:r1,r2...legacy} and are described\n  here: https://commons.apache.org/proper/commons-math/javadocs/api-3.3/index.html.\n\n  nan-strategy can be one of [:keep :remove :exception] and defaults to :exception."
  ([col percentiles]
   (let
-   [original-result__43654__auto__
+   [original-result__42776__auto__
     (tech.v3.datatype.functional/percentiles percentiles col)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43654__auto__)))
+    original-result__42776__auto__)))
  ([col percentiles options]
   (let
-   [original-result__43654__auto__
+   [original-result__42776__auto__
     (tech.v3.datatype.functional/percentiles percentiles options col)]
    (tablecloth.column.api.utils/return-scalar-or-column
-    original-result__43654__auto__))))
+    original-result__42776__auto__))))
 
