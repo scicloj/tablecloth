7ceeb40b11a12c07c8b3bc44316eae74b03c6a43
Ethan Miller <ethanzanemiller@gmail.com>
2023-03-17 11:54:12 -0400

    Activate test that wil now pass due to tmd upgrade

--- test/tablecloth/column/api/column_test.clj
+++ test/tablecloth/column/api/column_test.clj
@@ -20,11 +20,9 @@
       (-> [true false true]
           (column)
           (tech.v3.datatype/elemwise-datatype)) => :boolean
-      ;; disable this test until TC reaches 7.00-beta2
-      ;;(-> [1 true false]
-      ;;    (column)
-      ;;    (tech.v3.datatype/elemwise-datatype)) => :object
-      )
+      (-> [1 true false]
+         (column)
+         (tech.v3.datatype/elemwise-datatype)) => :object)
 
 (fact "`typeof` returns the concrete type of the elements"
       (typeof (column [1 2 3])) => :int64 
