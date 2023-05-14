695ecb89ba467464173cfd8ea19c320ede8b7717
Ethan Miller <ethanzanemiller@gmail.com>
2023-05-14 17:11:53 -0400

    Clean up

--- src/tablecloth/api/lift_operators.clj
+++ src/tablecloth/api/lift_operators.clj
@@ -1,10 +1,6 @@
 (ns tablecloth.api.lift-operators
   (:require [tablecloth.api :refer [select-columns]]))
 
-(require '[tablecloth.column.api.operators])
-
-(def ops-mappings (ns-publics 'tablecloth.column.api.operators))
-
 (defn get-arglists [fn-sym]
   (-> fn-sym resolve meta :arglists))
 
@@ -19,11 +15,6 @@
         col-symbol-set
         (set longest-arglist))))))
 
-
-;; broken if '[x y & args]
-(defn count-non-col-args [arglist]
-  (let [col-symbol-set #{'x 'y 'z}]
-    (count (clojure.set/difference (set arglist) col-symbol-set))))
 
 (defn convert-arglists [arglists]
   (let [convert-arglist
@@ -50,36 +41,3 @@
              (if (>= ~max-cols (count selected-cols#))
                (tablecloth.api.columns/add-or-replace-column ~'ds ~'target-col (apply ~fn-sym args-to-pass#))
                (throw (Exception. (str "Exceeded maximum number of columns allowed for operation."))))))))))
-
-(lift-op 'tablecloth.column.api.operators/shift)
-
-(eval (lift-op 'tablecloth.column.api.operators/shift))
-
-
-
-(apply tablecloth.column.api.operators/shift [[1 2 3] 1] )
-
-(tablecloth.column.api.operators/shift [1 2 3] 1)
-
-
-
-(comment 
-
-  (type #'tablecloth.column.api.operators/+)
-
-  (defn get-mappings []
-    (ns-publics 'tablecloth.column.api.operators))
-
-  (eval (lift-op 'tablecloth.column.api.operators/+))
-
-  (def ds (tablecloth.api/dataset {:a [1 2 3 4 5]
-                                   :b [6 7 8 9 10]
-                                   :c [11 12 13 14 16]}))
-
-
-
-
-  )
-
-
-
