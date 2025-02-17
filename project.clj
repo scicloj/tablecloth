da360e30a4158d8c5f3a40455e70a79b90aa9949
GenerateMe <generateme.blog@gmail.com>
2025-02-17 10:13:34 +0100

    deps up

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scicloj/tablecloth "7.030"
+(defproject scicloj/tablecloth "7.042"
   :description "Dataset manipulation library built on the top of tech.ml.dataset."
   :url "https://github.com/scicloj/tablecloth"
   :license {:name "The MIT Licence"
