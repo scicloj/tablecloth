e443d3a8c7f9ccef5b08fab0922e985c7b95fd59
GenerateMe <generateme.blog@gmail.com>
2026-05-07 10:13:06 +0200

    bump in lein

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scicloj/tablecloth "8.016"
+(defproject scicloj/tablecloth "8.021"
   :description "Dataset manipulation library built on the top of tech.ml.dataset."
   :url "https://github.com/scicloj/tablecloth"
   :license {:name "The MIT Licence"
